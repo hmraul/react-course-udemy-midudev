f008f45d3368e0f317634ee2eec80f39671c12ca
Raul Herranz Munoz <raul.herranz@scmspain.com>
2019-11-16 12:33:48 +0100

    Adding functions as prop

--- src/App.js
+++ src/App.js
@@ -18,18 +18,25 @@ class Hello extends Component {
 class Text extends Component {
   render() {
 
+    const {
+      complexObject,
+      doubleFunction,
+      numbers
+    } = this.props
     // const textDependsOn = this.props.boolean ? 'Cierto' : 'Falso';
-    const doubleNumbers = this.props.numbers.map(n => n * 2)
+    const doubleNumbers = numbers.map(n => n * 2)
+    const numbersFunction = numbers.map(doubleFunction)
 
     return (
       <div>
         {/* <p>{this.props.text}</p>
         <p>{this.props.number}</p>
         <p>{textDependsOn}</p> */}
-        <p>{this.props.complexObject.key1}</p>
-        <p>{this.props.numbers}</p>
-        <p>{this.props.numbers.join(',  ')}</p>
-        <p>{doubleNumbers.join(',  ')}</p>
+        <p>{complexObject.key1}</p>
+        <p>{numbers}</p>
+        <p>{numbers.join(', ')}</p>
+        <p>{doubleNumbers.join(', ')}</p>
+        <p>{numbersFunction.join(' - ')}</p>
       </div>
     )
   }
@@ -44,6 +51,7 @@ function App() {
         <Text 
           boolean
           complexObject = {{key1:'value1', key2:'value2'}}
+          doubleFunction = {(number) => number * 4}
           number={2} 
           numbers={[2, 3, 10]}
           text='Text from component' 
