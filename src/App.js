8287782aef01aa45145930d1047fa48e15dafbc5
Raul Herranz Munoz <raul.herranz@scmspain.com>
2019-12-30 18:03:13 +0100

    Events

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Bienvenidos al curso de react</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h3>Vamos a aprender React</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hola mundo, estoy usando react</p>
-        <strong>This is a strong</strong>
-      </header>
-    </div>
-  );
-}
 
-// class App extends Component {
-//   render () {
-//     return (
-//       <div className='App'>        
-//       </div>
-//     )
-//   }
-// }
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {mouseX: 0, mouseY: 0}
+    // Para enlazar el context, pero no es limpio
+    //this.handleMouseMove = this.handleMouseMove.bind(this)
+
+  }
+
+  handleClick(e) {
+    console.log(e)
+    // alert('Hi there')
+  }
+
+  //handleMouseMove(e) {
+  // Para enlazar mejor el contexto, usar arrow function
+  handleMouseMove = (e) => {
+    const {clientX, clientY} = e
+    this.setState({mouseX: clientX, mouseY: clientY})
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <h4>Eventos</h4>
+        <button onClick={this.handleClick}>Hi there</button>
+        <div
+          onMouseMove={this.handleMouseMove}
+          style={{border: '1px solid #000',
+          marginTop: 10,
+          padding: 10}}>
+          <p>{this.state.mouseX}, {this.state.mouseY}</p>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default App;
