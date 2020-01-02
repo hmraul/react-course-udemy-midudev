bb4f41b0a51aa93a8e90864a33850b874e50cd88
Raul Herranz Munoz <raul.herranz@scmspain.com>
2020-01-02 17:23:22 +0100

    Adding a Box

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Bienvenidos al curso de react</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h3>Vamos a aprender React</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hola mundo, estoy usando react</p>
-        <strong>This is a strong</strong>
-      </header>
+class Box extends Component {
+  render () {
+    return (<div
+    style = {{
+      border: '1px solid #000',
+      margin: 5,
+      padding: 5
+    }}>
+      {this.props.children}
     </div>
-  );
+    )}
 }
 
-// class App extends Component {
-//   render () {
-//     return (
-//       <div className='App'>        
-//       </div>
-//     )
-//   }
-// }
+
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <h4>Pobando Prop children</h4>
+        <Box>
+          Hola! Soy un children
+        </Box>
+        <Box>
+          Viva Murcia
+        </Box>       
+      </div>
+    )
+  }
+}
 
 export default App;
